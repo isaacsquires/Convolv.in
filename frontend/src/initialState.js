56a711eb5794508110608741b05db52c04a3bac8
isaacsquires <isaacsquires@me.com>
2022-07-05 13:55:27 +0100

    add ability to add dims to input and preserve

---
+++ frontend/src/initialState.js
@@ -0,0 +1,21 @@
+const initialState = {
+    'channelsSwitch': false,
+    input: {
+        size: [64,64],
+        dims: 2,
+        channels: 3,
+    },
+    layers: [
+        {
+            "k": 4,
+            "p": 2,
+            "s": 2,
+            'sf': 0,
+            'channels': 3,
+            "type": "conv2d",
+        },
+    ],
+    error: null
+};
+
+export default initialState;
